68c13c9778e9dc6100c7da01771a62a4d558bb6f
mambrocio <mikedbambrocio@gmail.com>
2023-06-01 18:16:11 -0400

    Javascript possibly needs fixing, but I fixed the responsive flex layouts

--- scripts/script.js
+++ scripts/script.js
@@ -26,39 +26,6 @@ var averageRent;
 //   });
 // });
 
-// Load the Visualization API and the corechart package.
-google.charts.load("current", { packages: ["corechart"] });
-
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart() {
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn("string", "Topping");
-  data.addColumn("number", "Slices");
-
-  data.addRows([
-    ["Mushrooms", 5],
-    ["Onions", 1],
-    ["Olives", 1],
-    ["Zucchini", 1],
-    ["Pepperoni", 2],
-  ]);
-
-  // Set chart options
-  var options = { title: "Current Spnding", width: 400, height: 300 };
-
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(
-    document.getElementById("chart_div")
-  );
-  chart.draw(data, options);
-}
-
 // Add an event listener to the form's submit event
 form.addEventListener("submit", function (event) {
   // Prevent the form from submitting
@@ -71,7 +38,6 @@ form.addEventListener("submit", function (event) {
   };
 
   var searchInput = document.getElementById("search-bar").value;
-  
   //the selected currency
   var currency = document.getElementById("displayCurrency").value;
 
@@ -204,7 +170,6 @@ form.addEventListener("submit", function (event) {
           console.log(exactCost);
 
           let badBudget = totalCost - income;
-          localStorage.setItem(searchInput, trueCostOfLiving);
 
           // resutlts cost of living in your city
           var generalCost = document.getElementById("city-cost");
